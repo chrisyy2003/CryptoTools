a85bb174764177b75c20866697f24de1e857ad9b
chris <1017975501@qq.com>
2020-05-08 17:39:11 +0800

    xor

---
+++ ClassicalCryptography/Xor/Hamming_distance.py
@@ -0,0 +1,81 @@
+import base64
+import string
+
+
+def bxor(a, b):     # xor two byte strings of different lengths
+    if len(a) > len(b):
+        return bytes([x ^ y for x, y in zip(a[:len(b)], b)])
+    else:
+        return bytes([x ^ y for x, y in zip(a, b[:len(a)])])
+
+
+def hamming_distance(b1, b2):
+    differing_bits = 0
+    for byte in bxor(b1, b2):
+        differing_bits += bin(byte).count("1")
+    return differing_bits
+
+
+def break_single_key_xor(text):
+    key = 0
+    possible_space = 0
+    max_possible = 0
+    letters = string.ascii_letters.encode('ascii')
+    for a in range(0, len(text)):
+        maxpossible = 0
+        for b in range(0, len(text)):
+            if(a == b):
+                continue
+            c = text[a] ^ text[b]
+            if c not in letters and c != 0:
+                continue
+            maxpossible += 1
+        if maxpossible > max_possible:
+            max_possible = maxpossible
+            possible_space = a
+    key = text[possible_space] ^ 0x20
+    return chr(key)
+
+
+b = base64.b64decode(open('11').read())
+
+
+normalized_distances = []
+
+for KEYSIZE in range(2, 40):
+    b1 = b[: KEYSIZE]
+    b2 = b[KEYSIZE: KEYSIZE * 2]
+    b3 = b[KEYSIZE * 2: KEYSIZE * 3]
+    b4 = b[KEYSIZE * 3: KEYSIZE * 4]
+    b5 = b[KEYSIZE * 4: KEYSIZE * 5]
+    b6 = b[KEYSIZE * 5: KEYSIZE * 6]
+
+    normalized_distance = float(
+        hamming_distance(b1, b2) +
+        hamming_distance(b2, b3) +
+        hamming_distance(b3, b4) +
+        hamming_distance(b4, b5) +
+        hamming_distance(b5, b6)
+    ) / (KEYSIZE * 5)
+    normalized_distances.append(
+        (KEYSIZE, normalized_distance)
+    )
+normalized_distances = sorted(normalized_distances, key=lambda x: x[1])
+
+
+for KEYSIZE, _ in normalized_distances:
+    block_bytes = [[] for _ in range(KEYSIZE)]
+    for i, byte in enumerate(b):
+        block_bytes[i % KEYSIZE].append(byte)
+    keys = ''
+    try:
+        for bbytes in block_bytes:
+            keys += break_single_key_xor(bbytes)
+        key = bytearray(keys * len(b), "utf-8")
+        plaintext = bxor(b, key)
+        print("size:" + "%-2d" % KEYSIZE, end='')
+        print("  key :", keys, "n")
+        # s = bytes.decode(plaintext)
+        # print(s)
+    except Exception:
+        continue
